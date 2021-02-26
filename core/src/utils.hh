34f1f994fec43265b50a7df9b84b9e7577653767
Johannes Dröge <code@fungs.de>
2021-02-26 21:18:52 +0100

    add copy of seqan print function

--- core/src/utils.hh
+++ core/src/utils.hh
@@ -118,7 +118,7 @@ void loadMapFromFile( const std::string& filename, std::map< KeyT, ValueT >& map
 	std::list< std::string > fields;
 	std::list< std::string >::iterator field_it;
 	std::ifstream file_handle( filename.c_str() );
-	
+
 	if( ! file_handle.good() ) {
 		std::cerr << "something wrong with file " << filename << std::endl;
 		file_handle.close();
@@ -155,7 +155,7 @@ typename ContainerT::iterator firstUnmaskedIter( ContainerT& cont ) {
 
 
 template< typename TType, unsigned int position >
-struct compareTupleFirstLT { 
+struct compareTupleFirstLT {
 	bool operator() ( const TType& a, const TType& b ) {
 		return boost::get<position>( a ) < boost::get<position>( b );
 	}
@@ -168,7 +168,7 @@ template< typename UIntType = small_unsigned_int > //default maximum 256 referen
 class ReferencedStringStore {
 	public:
 		ReferencedStringStore() : last_( id2counter_.end() ), max_capacity_( std::numeric_limits<UIntType>::max() ) {}
-		
+
 		const std::string& add( const std::string& id ) {
 			if ( &id != &last_->first ) {
 				last_ = id2counter_.find( id );
@@ -179,14 +179,14 @@ class ReferencedStringStore {
 			last_->second += 1;
 			return last_->first;
 		}
-		
+
 		bool remove( const std::string& id ) {
 			if ( &id != &last_->first ) {
 				typename std::map< const std::string, UIntType >::iterator it = id2counter_.find( id );
 				if ( it == id2counter_.end() || &id != &it->first ) return false;
 				last_ = it;
 			}
-			
+
 			if ( last_->second > 0 ) {
 				last_->second -= 1;
 			} else {
@@ -195,11 +195,110 @@ class ReferencedStringStore {
 			}
 			return true;
 		}
-		
+
 	private:
 		typename std::map< const std::string, UIntType > id2counter_; //max. 256 references
 		typename std::map< const std::string, UIntType >::iterator last_; //save some time
 		const UIntType max_capacity_;
 };
+
+// ----------------------------------------------------------------------------
+// Function write()
+// ----------------------------------------------------------------------------
+
+/*!
+ * @fn Align#write
+ * @deprecated Old-style I/O.
+ * @brief Writing of Gaps to Streams in human-readable format.
+ *
+ * @signature void write(stream, align);
+ *
+ * @param[in,out] stream The Stream to write to.
+ * @param[in]     align  The Align object to write out.
+ */
+
+// simplified seqan alignment print/write without without breaking it
+template <typename TFile, typename TSource, typename TSpec>
+inline void
+_write(TFile & target,
+      Align<TSource, TSpec> const & source)
+{
+    typedef Align<TSource, TSpec> const TAlign;
+    typedef typename Row<TAlign>::Type TRow;
+    typedef typename Position<typename Rows<TAlign>::Type>::Type TRowsPosition;
+    typedef typename Position<TAlign>::Type TPosition;
+
+    TRowsPosition row_count = length(rows(source));
+    TPosition begin_ = 0;
+    TPosition end_ = std::min(length(row(source, 0)), length(row(source, 1)));
+
+    unsigned int baseCount = 0;
+    unsigned int leftSpace = 6;
+    while (begin_ < end_)
+    {
+        unsigned int windowSize_ = 50;
+        if ((begin_ + windowSize_) > end_)
+            windowSize_ = end_ - begin_;
+
+        // Print header line
+        char buffer[20];
+        int len = snprintf(buffer, 20, "%7u", (unsigned)baseCount);
+        write(target, buffer, len);
+        baseCount += windowSize_;
+        writeValue(target, ' ');
+        for (TPosition i = 1; i <= windowSize_; ++i)
+        {
+            if ((i % 10) == 0)
+                writeValue(target, ':');
+            else if ((i % 5) == 0)
+                writeValue(target, '.');
+            else
+                writeValue(target, ' ');
+        }
+        writeValue(target, ' ');
+        writeValue(target, '\n');
+
+        // Print sequences
+        for (TRowsPosition i = 0; i < 2 * row_count - 1; ++i)
+        {
+            for (unsigned int j = 0; j < leftSpace + 2; ++j)
+                writeValue(target, ' ');
+            if ((i % 2) == 0)
+            {
+                TRow & row_ = row(source, i / 2);
+                typedef typename Iterator<typename Row<TAlign>::Type const, Standard>::Type TIter;
+                TIter begin1_ = iter(row_, begin_);
+                TIter end1_ = iter(row_, begin_ + windowSize_);
+                for (; begin1_ != end1_; ++begin1_)
+                {
+                    if (isGap(begin1_))
+                        writeValue(target, gapValue<char>());
+                    else
+                        writeValue(target, getValue(begin1_));
+                }
+            }
+            else
+            {
+                for (unsigned int j = 0; j < windowSize_; ++j)
+                {
+                    if ((!isGap(row(source, (i - 1) / 2), begin_ + j)) &&
+                        (!isGap(row(source, (i + 1) / 2), begin_ + j)) &&
+                        (row(source, (i - 1) / 2)[begin_ + j] == row(source, (i + 1) / 2)[begin_ + j]))
+                    {
+                        writeValue(target, '|');
+                    }
+                    else
+                    {
+                        writeValue(target, ' ');
+                    }
+                }
+            }
+            writeValue(target, '\n');
+        }
+        writeValue(target, '\n');
+        begin_ += 50;
+    }
+    writeValue(target, '\n');
+}
 
 #endif // utils_hh_
